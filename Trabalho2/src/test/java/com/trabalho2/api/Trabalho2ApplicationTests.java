ee5c871e06e498a08f7af28b6346bd5fb42aeb66
JulioBrych <63235921+JulioBrych@users.noreply.github.com>
2020-09-25 09:34:20 -0300

    Ta funcinando porra

---
+++ Trabalho2/src/test/java/com/trabalho2/api/Trabalho2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho2.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalho2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
